ec16f103e02b376978b8a88ad16a25c6d584862b
Ander Kamada <anderkamada3@gmail.com>
2025-06-24 20:49:24 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { Pool } = require("pg");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // conexão com o bancoAdd commentMore actions
 const pool = new Pool({
